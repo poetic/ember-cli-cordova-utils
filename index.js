d73926875d2dbffa8cefe4db14bbbc22893921a1
Jake Craige <jake@poeticsystems.com>
2014-09-09 11:32:48 -0500

    refactor index.js

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-var fs = require('fs');
-var path = require('path');
-
-function CordovaUtils(project) {
-  this.project = project;
-  this.name = 'Ember CLI Cordova Utils';
-}
-
-function unwatchedTree(dir) {
-  return {
-    read:    function() { return dir; },
-    cleanup: function() { }
-  };
-}
-
-CordovaUtils.prototype.treeFor = function(name) {
-  var treePath = path.join(__dirname, name);
-
-  if (fs.existsSync(treePath)) {
-    return unwatchedTree(treePath);
-  }
+module.exports = {
+  name: 'Ember CLI Cordova Utils'
 };
-
-module.exports = CordovaUtils;
